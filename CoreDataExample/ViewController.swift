cbeb1adebb3042556cde74705f33af3f4b75f77e
Ahmet Mucahid Bozkurt <ahmetmb46@gmail.com>
2022-05-02 15:45:56 +0300

    Initial Commit

---
+++ CoreDataExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by Ahmet Mucahid Bozkurt on 2.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
